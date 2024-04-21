3b561db71d3d1cda94d11257726f2ac931f1ca36
rush181200 <rushabhm75@gmail.com>
2024-04-21 03:34:52 -0400

    again

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -47,6 +47,7 @@ const Hero = () => {
                 Download CV
                 <Download size={18} />
               </Button> */}
+              
             <a href="https://drive.google.com/file/d/1GyRoZV49b54bTN7nKqU0wU949GeajxOZ/view?usp=drive_link" download>Download CV</a>
              
              
